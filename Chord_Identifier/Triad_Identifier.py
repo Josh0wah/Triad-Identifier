0407654d4809858b6fd494c5146a9f58d9a80cee
Josh0wah <134548428+Josh0wah@users.noreply.github.com>
2023-09-21 22:59:24 -0500

    Update Triad_Identifier.py

--- Chord_Identifier/Triad_Identifier.py
+++ Chord_Identifier/Triad_Identifier.py
@@ -1,47 +1,58 @@
-# inputs
-root = input("Enter root note: ")
-note2 = input("Enter second note: ")
-note3 = input("Enter third note: ")
 
-# Lists
-all_notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-# nfr = "notes from root"
-nfr = [
-    root,
-    all_notes[(all_notes.index(root) + 1) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 2) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 3) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 4) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 5) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 6) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 7) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 8) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 9) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 10) % (len(all_notes))],
-    all_notes[(all_notes.index(root) + 11) % (len(all_notes))],
-    root]
+keep_going = "1"
+
+while keep_going == "1":
+
+    # inputs
+    root = input("Enter root note: ").upper()
+    note2 = input("Enter second note: ").upper()
+    note3 = input("Enter third note: ").upper()
+
+    # Lists
+    all_notesS = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    #all_notesF = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+
+    # nfr = "notes from root"
+    nfr = [
+        root,
+        all_notesS[(all_notesS.index(root) + 1) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 2) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 3) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 4) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 5) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 6) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 7) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 8) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 9) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 10) % (len(all_notesS))],
+        all_notesS[(all_notesS.index(root) + 11) % (len(all_notesS))],
+        root]
 
 
-# Functions that identify triads
-def triad_identity():
-    if nfr.index(note2) == 4 and nfr.index(note3) == 7:
-        return root + " major chord"
-    elif nfr.index(note2) == nfr.index(root) + 2 and nfr.index(note3) == nfr.index(root) + 7:
-        return root + " sus2 chord"
-    elif nfr.index(note2) == nfr.index(root) + 5 and nfr.index(note3) == nfr.index(root) + 7:
-        return root + " sus4 chord"
-    elif nfr.index(note2) == nfr.index(root) + 4 and nfr.index(note3) == nfr.index(root) + 8:
-        return root + " augmented chord"
-    elif nfr.index(note2) == 3 and nfr.index(note3) == 6:
-        return root + " diminished triad"
-    elif nfr.index(note2) == 3 and nfr.index(note3) == 7:
-        return root + " minor triad"
+    # Functions that identify triads
+    def triad_identity():
+        if nfr.index(note2) == 4 and nfr.index(note3) == 7:
+            return root + " major chord"
+        elif nfr.index(note2) == nfr.index(root) + 2 and nfr.index(note3) == nfr.index(root) + 7:
+            return root + " sus2 chord"
+        elif nfr.index(note2) == nfr.index(root) + 5 and nfr.index(note3) == nfr.index(root) + 7:
+            return root + " sus4 chord"
+        elif nfr.index(note2) == nfr.index(root) + 4 and nfr.index(note3) == nfr.index(root) + 8:
+            return root + " augmented chord"
+        elif nfr.index(note2) == 3 and nfr.index(note3) == 6:
+            return root + " diminished chord"
+        elif nfr.index(note2) == 3 and nfr.index(note3) == 7:
+            return root + " minor chord"
+        else:
+            return "Not recognized"
+
+    keep_going = "1"
+
+    # Run functions
+
+    if root in nfr:
+        print(triad_identity())
     else:
-        return "Not recognized"
-
-
-# Run functions
-if root in nfr:
-    print(triad_identity())
-else:
-    print("Error")
+        print("Error")
+        
+    keep_going = input("Type 1 to run again: ")
